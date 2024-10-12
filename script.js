02170d60952842d450b9a168adbf20e260f7e5d1
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-12 22:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function calculateMinCost() {
-  const roper= document.getElementById("rope-lengths")
-	let lenarr= (roper.value).split(',')
-	let sum=0;
-	let fs= lenarr.map(x => Number(x))
-	let gh=0
-	while(fs.length!=1){
-		fs.sort(function(a,b){return a-b})
-		sum=fs[0]+fs[1]
-		fs[1]=sum
-		fs.shift()
-		gh=gh+sum
-	}
-	const reser= document.getElementById("result")
-	reser.innerText= gh
-}  
+function mincost(arr) {
+    let MinCostPrice = 0;
+
+    function mincost1(arr) { 
+        if(arr.length === 1){
+            return MinCostPrice;
+        }
+        else {
+            let CostArray = [];
+            SortedArray = arr.sort((a,b)=>a-b);
+            CostArray[0] = SortedArray[0] + SortedArray[1];
+            MinCostPrice += CostArray[0];
+            for(let i = 1; i < SortedArray.length - 1; i++){
+                CostArray[i] = SortedArray[i+1];
+            }
+            return mincost1(CostArray);
+        }
+    }
+
+    return mincost1(arr);
+}
+
+module.exports = mincost;
