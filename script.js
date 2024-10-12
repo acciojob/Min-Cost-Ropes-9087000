a4c3597b7820cbeec58decd8af614572c68d4fcc
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-12 21:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    // Create a Min Heap (Priority Queue)
+    const minHeap = new MinPriorityQueue();
+    
+    // Add all ropes to the Min Heap
+    arr.forEach(rope => minHeap.enqueue(rope));
+    
+    let totalCost = 0;
+    
+    // While there is more than one rope, keep connecting the shortest two
+    while (minHeap.size() > 1) {
+        // Extract the two shortest ropes
+        let first = minHeap.dequeue().element;
+        let second = minHeap.dequeue().element;
+        
+        // Cost of connecting the two ropes
+        let cost = first + second;
+        totalCost += cost;
+        
+        // Add the new combined rope back to the heap
+        minHeap.enqueue(cost);
+    }
+    
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
